f8cd12dc113c0a3fc6731e0459a1d2f37e6e9714
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2023-09-23 14:21:30 +0500

    Update Linear_Regression.py

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,4 +1,5 @@
 from sympy import *
+import time
 
 class LR:
     def __init__(self,x,y,dimension):
@@ -45,43 +46,78 @@ class LR:
     
         return dr
 
-    def GD(self,loops):
-        l_r = 0.01
+    def GD(self,loops,out=False):
+        l_r = 0.001
         for _ in range(loops):
             for i in range(self.dimension):
                 slope = self.derivative(f"m{i}")
                 step = slope*l_r
-                self.m[i] += step
+                self.m[i] -= step
             
             slope = self.derivative("b")
             step = slope*l_r
-            self.b += step 
+            self.b -= step 
 
             l_r = l_r/1.0001
-
+        if out == True:
+            return (self.m,self.b)
         return
     
-    def predict(self,value):
-        e = symbols("b")
-        for j in range(self.dimension):
-            m = symbols(f"m{j}")
-            x = symbols(f"x{j}")
-            e += m*x
+    def predict(self,value,custom=False,m_l = None,b_l = None):
+        if custom == True:
+            e = symbols("b")
+            for j in range(self.dimension):
+                m = symbols(f"m{j}")
+                x = symbols(f"x{j}")
+                e += m*x
 
-        subss = {"b":self.b}
-        for j in range(self.dimension):
-            subss[f"m{j}"]=self.m[j]
-            subss[f"x{j}"]=value[j]
+            subss = {"b":b_l}
+            for j in range(self.dimension):
+                subss[f"m{j}"]=m_l[j]
+                subss[f"x{j}"]=value[j]
 
-        return e.subs(subss)
+            return e.subs(subss)
+        elif custom == False:
+            e = symbols("b")
+            for j in range(self.dimension):
+                m = symbols(f"m{j}")
+                x = symbols(f"x{j}")
+                e += m*x
+
+            subss = {"b":self.b}
+            for j in range(self.dimension):
+                subss[f"m{j}"]=self.m[j]
+                subss[f"x{j}"]=value[j]
+
+            return e.subs(subss)
 
 
     
-x = [[2,2],[5,5],[1,1],[3,3],[4.5,4.5]]
-y = [2.5,4.8,0.9,3.3,4.1]
+x = [[2],
+     [5],
+     [1],
+     [3],
+     [4.5],
+     [5.3],
+     [5.8],
+     [7.8],
+     [9]]
+y = [2.5,
+     4.8,
+     0.9,
+     3.3,
+     4.1,
+     5.5,
+     6.2,
+     8,
+     8.6]
 
-model = LR(x,y,3)
+model = LR(x,y,2)
 print(model.SSE())
-print(model.GD(100))
+print(model.predict([2]))
+start = time.time()
+print(model.GD(200,out=True))
+end = time.time()
+print("time : ",end-start)
 print(model.SSE())
-print(model.predict([2,2]))
+print(model.predict([2]))
