b3b854da6c0f7c22ec44cdd99e8bb0b252258403
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2023-10-01 22:31:50 +0500

    Update logistic_regression_SKlearn.py

--- logistic_regression_SKlearn.py
+++ logistic_regression_SKlearn.py
@@ -4,20 +4,20 @@ import numpy as np
 
 # hour / 8, practice / 5, test / 15, attendance / 260, practical / 10
 
-data_x_train = np.array([[3,1,8,220,6],[4.5,2.3,11,234,7],[2,0.5,6,160,4],[6,3,13,240,8],[2,1,8,200,6],[3.5,1.3,9,190,7]])
-data_x_test = np.array([[3,1,5,140,4],[5,3.6,13,186,7],[1,0.3,4,150,5],[6.3,3.2,12,220,9],[3,2,9,190,6],[3,2.1,8,183,5]])
+data_x_train = np.array([[3],[4.5],[2],[6],[2],[3.5]])
+# data_x_test = np.array([[3,1,5,140,4],[5,3.6,13,186,7],[1,0.3,4,150,5],[6.3,3.2,12,220,9],[3,2,9,190,6],[3,2.1,8,183,5]])
 
 data_y_train = np.array([0,1,0,1,1,1])
-data_y_test = np.array([0,1,0,1,1,0])
+# data_y_test = np.array([0,1,0,1,1,0])
 
 model = LogisticRegression()
 
 model.fit(data_x_train,data_y_train)
 
-y_predict = model.predict(np.array(data_x_test))
+# y_predict = model.predict(np.array(data_x_test))
 
 # print(y_predict)
-print(log_loss(data_y_test,y_predict))
+# print(log_loss(data_y_test,y_predict))
 print(model.coef_)
 print(model.intercept_)
 
